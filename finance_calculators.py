a38cc3b126cad45cbb44ec50fafc79ac4d026be3
Maria Karagianni <154323531+MaKaragianni@users.noreply.github.com>
2024-03-03 18:57:52 +0000

    Add files via upload

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,62 +1,40 @@
-'''
-We'll create an investment calculator & a home loan repayment calculation program
-The user has 2 options to choose from (investment or bond)
-
-Issue/Change 2 to existing program: Adding Constants for User Prompts and Messages
-Coding strings directly into the code can make it challenging to make changes later on,
-especially for repeated messages or prompts. Defining these strings as constants
-at the beginning of the script can improve maintainability.
-'''
+# This is a Capstone project
+# We'll create an investment calculator & a home loan repayment calculation program
+# The user has 2 options to choose from (investment or bond)
 
 import math
 
-# Constants
-INVESTMENT_PROMPT = "Enter 'investment' if you wish to calculate the amount of interest you'll earn on your investment"
-BOND_PROMPT = "Enter 'bond' if you wish to calculate the amount you'll have to pay on a home loan"
-CHOICE_PROMPT = "Please enter either 'investment' or 'bond' from the menu above to proceed: "
-INVALID_INPUT_MESSAGE = "Invalid input. Please enter either 'investment' or 'bond'."
-DEPOSIT_PROMPT = "Please enter the amount of your deposit: "
-INTEREST_RATE_PROMPT = "Please enter your interest rate, as a percentage (e.g., enter 8 for 8%): "
-YEARS_PROMPT = "Please enter the number of years you plan on investing: "
-INTEREST_TYPE_PROMPT = "Would you prefer 'simple' or 'compound' interest? "
-HOUSE_VALUE_PROMPT = "Please enter the current value of the house (e.g., 1000000): "
-ANNUAL_INTEREST_PROMPT = "Please enter the interest rate as a percentage (e.g., enter 7 for 7%): "
-MONTHS_PROMPT = "Please enter the number of months over which you plan to repay the bond (e.g., 120): "
+print("There are two types of calculations available: ")
+print("Enter 'investment' if you wish to calculate the amount of interest you'll earn on your investment")
+print("Enter 'bond' if you wish to calculate the amount you'll have to pay on a home loan")
+choice = input("Please enter either 'investment' or 'bond' from the menu above to proceed: ").lower()
+    
+if choice == "investment": # If the user enters 'investment', the below user input is requested
+    p = float(input("Please enter the amount of your deposit: "))
+    r = float(input("Please enter your interest rate, as a percentage (e.g., enter 8 for 8%): ")) / 100
+    t = int(input("Please enter the number of years you plan on investing: "))
+    interest = input("Would you prefer 'simple' or 'compound' interest? ").lower() # The user has 2 options for interest (simple or compound)
 
-def calculate_investment(p, r, t, interest):
     if interest == "simple":
-        return p * (1 + r * t)
+        a = p * (1 + r * t)
     elif interest == "compound":
-        return p * math.pow((1 + r), t)
+        a = p * math.pow((1 + r),t)
     else:
         print("Invalid interest type. Please enter 'simple' or 'compound'.")
-        return None
 
-def calculate_bond(p, annual_interest, n):
-    i = annual_interest / 12  # Converting annual interest rate to monthly
-    return (i * p) / (1 - (1 + i) ** (-n))
+# We can then calculate the output is the amount they'll get back after the given period at the given interest rate
+    print(f"The total amount after {t} years is: {a}")
 
-# Main program
-print("There are two types of calculations available: ")
-print(INVESTMENT_PROMPT)
-print(BOND_PROMPT)
+elif choice == "bond": # If the user enters 'bond', the below user input is requested
+    p = float(input("Please enter the current value of the house (e.g., 1000000): "))
+    annual_interest = float(input("Please enter the interest rate as a percentage (e.g., enter 7 for 7%): ")) / 100
+    n = int(input("Please enter the number of months over which you plan to repay the bond (e.g., 120): "))
+    
+    i = annual_interest / 12 # Converting annual interest rate to monthly
+    repayment = (i * p) / (1 - (1 + i) ** (-n))
 
-choice = input(CHOICE_PROMPT).lower()
-
-if choice == "investment":
-    p = float(input(DEPOSIT_PROMPT))
-    r = float(input(INTEREST_RATE_PROMPT)) / 100
-    t = int(input(YEARS_PROMPT))
-    interest = input(INTEREST_TYPE_PROMPT).lower()
-    total_amount = calculate_investment(p, r, t, interest)
-    if total_amount is not None:
-        print(f"The total amount after {t} years is: {total_amount}")
-
-elif choice == "bond":
-    p = float(input(HOUSE_VALUE_PROMPT))
-    annual_interest = float(input(ANNUAL_INTEREST_PROMPT)) / 100
-    n = int(input(MONTHS_PROMPT))
-    repayment = calculate_bond(p, annual_interest, n)
+# We can then calculate how much money the user will have to repay each month
     print(f"Your monthly bond repayment will be: {repayment}")
+
 else:
-    print(INVALID_INPUT_MESSAGE)
+    print("Invalid input. Please enter either 'investment' or 'bond'.")
